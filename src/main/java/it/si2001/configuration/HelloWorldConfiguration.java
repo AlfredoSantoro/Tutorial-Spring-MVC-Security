f1e8e09cbedbfd89d36c6641b546f2fce86a2d45
Unknown <alfredosantoro.informatico@gmail.com>
2018-05-25 17:04:29 +0200

    helloworldspringmvcinit

---
+++ src/main/java/it/si2001/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,41 @@
+package it.si2001.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/*
+* i bean e le dipendenze che hanno, sono riflesse nelle configurazioni le quali sono
+* utilizzate dal container IoC di spring
+* Spring ottiene le informazioni su come istanziare e configurare gli oggetti attraverso
+* file di configurazione
+*
+* @EnableWebMvc invece dà supporto ai controller nell'utilizzare @RequestMapping
+* @ComponentScan invece indica a spring dove cercare le classi da scannerizzare e gestire nel container
+* in funzione di un basePackages
+* */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "it.si2001")
+public class HelloWorldConfiguration {
+
+
+    /*stiamo definendo un bean il quale sarà gestito dal container di spring. Il compito di questo benan è
+     quello di definire un risolutore di view il quale definisce come le viste indicate dai controller
+     devono essere definite. Nel nostro caso sono delle JSTL con estensione jsp. */
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
---
+++ src/main/java/it/si2001/configuration/HelloWorldInit.java
@@ -0,0 +1,41 @@
+package it.si2001.configuration;
+
+
+
+
+/*
+Al bootstrap del servlet container, verrà istanziata questa classe e verrà eseguito il metodo startUp
+in cui indichiamo a spring il dispatcher servlet ovvero il front controller e gli indichiamo la classe
+di configurazione di spring
+ */
+
+
+// utilizziamo questa implementazione perchè è più coincisa. Viene definito il file di configurazione
+// e viene definito il SelvetMapping del front controller il quale indica che gestirà ogni richiesta.
+
+
+/*
+NB. la configurazione basata sulle annotazioni java, dipende dal servlet container 3.0. Quindi dobbiamo assicurarci
+di non avere nessun file xml
+ */
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class HelloWorldInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ HelloWorldConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
